c491567cfafd22d568eaf91832d0c641d82f4db1
廣田真由 <my-hrt@i.softbank.jp>
2019-12-06 15:26:54 +0900

    現場Railsのコードをgit管理に変更

---
+++ spec/tasks_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "タスク管理機能", type: :system do
+  describe "一覧表示機能" do
+    before do
+    end
+    context "ユーザーAがログインしているとき" do
+      before do
+      end
+
+      if "ユーザーAが作成したタスクが表示される" do
+      end
+    end
+  end
+end
